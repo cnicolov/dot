cc956b8a6ac7802ab4150486bfadd2f010582d8f
Kristiyan Nikolov <knikolovx@gmail.com>
2018-01-21 23:22:00 +0200

    Rename tests

---
+++ tests/01-dotfiles.py
@@ -0,0 +1,37 @@
+import unittest
+import subprocess
+import os.path
+
+programs = [
+  'ansible',
+  'terraform',
+  'nvim',
+  'tree',
+  'aws',
+  'vagrant',
+  'virtualbox',
+]
+
+links = [
+  '~/.zshrc',
+  '~/.zshenv',
+  '~/.zshaliases',
+  '~/.zshfunctions',
+  '~/.config/nvim',
+]
+
+class DotfilesTest(unittest.TestCase):
+
+  def test_programs_installed(self):
+    for program in programs:
+      with self.subTest(program=program):
+        proc = subprocess.Popen(['which', program])
+        proc.communicate()
+        self.assertEqual(proc.returncode, 0)
+
+  def test_links_placed(self):
+    for link in links:
+      with self.subTest(link=link):
+        link_path = os.path.expanduser(link)
+        dir_or_file = os.path.isfile(link_path) or os.path.isdir(link_path)
+        self.assertTrue(dir_or_file and os.path.islink(link_path))
